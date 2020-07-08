0447f42306fd1f3200cfcb3ee4f47cdb73e2fe61
Andres Castro <lider.desarrollo@prime-consultores.com>
2020-07-08 11:51:35 -0500

    Add KartPackage

---
+++ GetaClubIo_Test/Assets/AndresCastroAssets/Scripts/AGameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AGameManager : MonoBehaviour
+{
+	public GameType gameType;
+
+	public void Start()
+	{
+		if (gameType == GameType.timer)
+		{
+
+		}
+	}
+}
+
+public enum GameType
+{
+	timer,
+	vs
+}
